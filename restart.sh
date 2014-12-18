4b2850952421bb0ef193dbc5560b7cfb04daba91
rinh <hao.lin@qunar.com>
2014-12-18 12:15:35 +0800

    update shell

--- restart.sh
+++ restart.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 path=$(cd $(dirname $0);echo $PWD)
 
-[[ -e $path/pid.nohup ]] && kill -QUIT `cat $path/pid.nohup`
-sleep 2
-nohup sudo node $path/app.js >/dev/null 2>&1 &
-echo $! > $path/pid.nohup
+#[[ -e $path/pid.nohup ]] && kill -QUIT `cat $path/pid.nohup`
+#sleep 2
+#nohup sudo node $path/app.js >/dev/null 2>&1 &
+#echo $! > $path/pid.nohup
+sudo pm2 restart fekit-package-www
